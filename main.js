f82078b7682bef155284cfe6ff33fabd7fa582e7
RRiverWest <kawa.ryo.0126@gmail.com>
2024-08-18 10:34:24 +0900

    add led flash from http request

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 const raspi = require('raspi');
 const gpio = require('raspi-gpio');
 const sleep = require('sleep');
+const express = require("express");
+const app = express();
 
-const interval = 2000;
+app.listen(3000);
+
+const interval = 1000;
 const led = new gpio.DigitalOutput('GPIO21');
+// connect led to gpio21(40pin)
 
-raspi.init(() => {
-  while (true) {
+app.get("/", (req, res) => {
+  for (let i = 0; i < 5; i++) {
+
     led.write(gpio.HIGH);
     sleep.msleep(interval);
 
     led.write(gpio.LOW);
     sleep.msleep(interval);
   }
+  console.log("ok");
 });
